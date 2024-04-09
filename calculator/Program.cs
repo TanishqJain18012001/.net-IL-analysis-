99fd5c13a1aa107f357a6d09aeedb625a8ba03b4
Tanishq Jain <Tanishq.J_INT1581@accolite.com>
2024-04-10 01:43:35 +0530

    initial

---
+++ calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SimpleAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number:");
+            int num2 = int.Parse(Console.ReadLine()); ;
+            int sum = addition.Add(num1, num2);
+            Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+        }
+        class addition
+        {
+            public static int Add(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
